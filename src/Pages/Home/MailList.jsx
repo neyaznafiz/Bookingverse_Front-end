69475997c60a65fc45a504225930dba44b6b3e3e
neyaznafiz <niiyazobug@gmail.com>
2022-12-16 00:09:46 +0600

    mailList component done

--- src/Pages/Home/MailList.jsx
+++ src/Pages/Home/MailList.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const MailList = () => {
   return (
-    <div className="w-full mt-12">
-      <h1>Save Time, Save Money!</h1>
-      <p>Sign Up and we'll send the best deals to you</p>
-      <div>
-        <input type="text" placeholder="Your E-mail" />
-        <button>Subscribe</button>
+    <div className="w-full mt-12 bg-primary text-white flex flex-col items-center gap-5 p-12">
+      <h1 className="text-5xl">Save Time, Save Money !</h1>
+      <p className="tracking-wide">Sign Up and we'll send the best deals to you</p>
+      <div className="flex items-center gap-x-2">
+        <input type="text" placeholder="Your E-mail" className="w-72 h-8 p-2 outline-none rounded-sm text-gray-800 "/>
+        <button className="bg-white h-8 text-primary px-2 rounded-sm">Subscribe</button>
       </div>
     </div>
   );
