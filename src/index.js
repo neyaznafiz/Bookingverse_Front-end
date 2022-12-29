d0761d8b800ddfabc2d8dde703da9cb1626d8962
neyaznafiz <niiyazobug@gmail.com>
2022-12-29 15:36:45 +0600

    search context created

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './global.css';
 import App from './App';
+import { SearchContextProvider } from './Context/SearchContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <SearchContextProvider>
     <App />
+    </SearchContextProvider>
   </React.StrictMode>
 );
 
